3a1ec8487106fbfb65c8139533598ad7d5c1e6ce
guidedgateway <rsaha@xmapledatalab.com>
2015-07-20 07:23:30 -0700

    real commit

---
+++ guide_home.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>	Guided Gateway - Affordable Authentic India Tours</title>
+	<meta name="description" content="Guided Gateway" >
+	<meta name="author" content="xMaple">
+
+	<!-- Mobile Specific Meta -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+
+	<!-- Bootstrap  -->
+	<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+	
+	
+	<!-- icon fonts font Awesome -->
+	<link href="assets/css/font-awesome.min.css" rel="stylesheet">
+
+	<!-- Custom Styles -->
+	<link href="assets/css/style.css" rel="stylesheet">
+
+	<!--[if lt IE 9]>
+	<script src="assets/js/html5shiv.js"></script>
+	<![endif]-->
+
+	<style>
+			#usernmae::-moz-placeholder 
+			{
+			  color:    #909090;
+			}
+			#password::-moz-placeholder 
+			{
+			  color:    #909090;
+			}
+			.mycontainer{
+				padding: 0px 30px 0px 30px;
+				}
+			
+			</style>
+</head>
+<body>
+
+
+	<!-- Preloader -->
+	<div id="preloader">
+		<div id="loader">
+			<div class="dot"></div>
+			<div class="dot"></div>
+			<div class="dot"></div>
+			<div class="dot"></div>
+			<div class="dot"></div>
+			<div class="dot"></div>
+			<div class="dot"></div>
+			<div class="dot"></div>
+			<div class="lading"></div>
+		</div>
+	</div><!-- /#preloader -->
+	<!-- Preloader End-->
+
+	<!-- Page Top Section -->
+	<section id="page-top" class="section-style" data-background-image="images/background/page-top.jpg">
+		<div class="pattern height-resize">
+			<div class="container mycontainer">
+			<div class="row">
+				<div class="col-sm-7 col-sm-offset-3" >
+					<div class="panel panel-default">
+						<div class="panel-body "  style="background-color:#dddddd;">
+							<div class="text-center"><br />
+								<a style="color: black; font-size:47px; font-weight:bold; font-family:Pacifico; text-decoration:none;">Guided Gateway</a>
+							</div>
+							<br /><br />
+							<div class="col-sm-10 col-sm-offset-1">
+								<form action="#" method="post">
+									<input type="email" class="form-control" id="usernmae" name="usernmae" placeholder="Email Address or Mobile Number" style="height:40px; background-color:white; color:black; -moz-border-radius: 10px; border-radius: 3px; border:solid 1px gray; padding:5px;">
+									<br /> <input type="password" class="form-control" id="password" name="password" placeholder="Password" style="height:40px; background-color:white; color:black; -moz-border-radius: 10px; border-radius: 3px; border:solid 1px gray; padding:5px;">
+									<br /> <button class="btn  btn-sm btn-default btn-block" style="height:40px; -moz-border-radius: 10px; border-radius: 3px; padding:5px; background-color:#ff845e; font-size:larger;" type="submit">login</button>
+								</form>
+								<br /> <center><span style="color:gray;">Not a member? <a href="RegistrationPage.php" style="color:#ff845e">Join now</a></span></center>
+								
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<br /><br /><br /><br /><br />
+				
+			</div><!-- /.container -->
+		</div><!-- /.pattern -->		
+	</section><!-- /#page-top -->
+	<!-- Page Top Section  End -->
+
+<!-- Modal -->
+<div id="SignUpModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title" style="color:black;">Please Fill the detail below -</h4>
+      </div>
+	  
+	  
+	<div class="modal-body">
+	<center>
+		<table>
+		<tr>
+		<td><span style="color:black;">First Name : <span></td>
+		<td width="10px"></td>
+		<td><input type="text" id="fname" class="form-control" style="color:black; -moz-border-radius: 15px; border-radius: 10px; border:solid 1px gray; padding:5px;" placeholder="First Name"></td>
+		</tr>
+		<tr><td height="5px"></td></tr>
+		<tr>
+		<td><span style="color:black;">Last Name : <span></td>
+		<td width="10px"></td>
+		<td><input type="text" id="fname" class="form-control" style="color:black; -moz-border-radius: 15px; border-radius: 10px; border:solid 1px gray; padding:5px;" placeholder="First Name"></td>
+		</tr>
+		<tr><td height="5px"></td></tr>
+		<tr>
+		<td><span style="color:black;">Email ID : <span></td>
+		<td width="10px"></td>
+		<td><input type="text" id="fname" class="form-control" style="color:black; -moz-border-radius: 15px; border-radius: 10px; border:solid 1px gray; padding:5px;" placeholder="First Name"></td>
+		</tr>
+		<tr><td height="5px"></td></tr>
+		<tr>
+		<td><span style="color:black;">Mobile Number : <span></td>
+		<td width="10px"></td>
+		<td><input type="text" id="fname" class="form-control" style="color:black; -moz-border-radius: 15px; border-radius: 10px; border:solid 1px gray; padding:5px;" placeholder="First Name"></td>
+		</tr>
+		<tr><td height="5px"></td></tr>
+		<tr>
+		<td><span style="color:black;">Choose Password : <span></td>
+		<td width="10px"></td>
+		<td><input type="password" id="fname" class="form-control" style="color:black; -moz-border-radius: 15px; border-radius: 10px; border:solid 1px gray; padding:5px;" placeholder="First Name"></td>
+		</tr>
+		<tr><td height="5px"></td></tr>
+		<tr>
+		<td><span style="color:black;">Re enter Password : <span></td>
+		<td width="10px"></td>
+		<td><input type="password" id="fname" class="form-control" style="color:black; -moz-border-radius: 15px; border-radius: 10px; border:solid 1px gray; padding:5px;" placeholder="First Name"></td>
+		</tr>
+		<tr><td height="25px"></td></tr>
+		<tr>
+		<td></td>
+		<td width="10px"></td>
+		<td><button type="button" class="btn btn-default" data-dismiss="modal">Register</button></td>
+		</tr>
+		</table>
+		</center>
+	</div>
+    </div>
+  </div>
+</div>
+	
+		<!-- Footer Section -->
+		<footer id="footer-section">
+			<p class="copyright">
+				&copy; <a href="index.php">Guided Gateway</a> 2014-2015, All Rights Reserved.
+			</p>
+		</footer>
+		<!-- Footer Section End -->
+
+
+		<!-- jQuery Library -->
+		<script type="text/javascript" src="assets/js/jquery-2.1.0.min.js"></script>
+		<!-- Modernizr js -->
+		<script type="text/javascript" src="assets/js/modernizr-2.8.0.min.js"></script>
+		<!-- Plugins -->
+		<script type="text/javascript" src="assets/js/plugins.js"></script>
+		<!-- Custom JavaScript Functions -->
+		<script type="text/javascript" src="assets/js/functions.js"></script>
+		<!-- Custom JavaScript Functions -->
+		<script type="text/javascript" src="assets/js/jquery.ajaxchimp.min.js"></script>
+
+	</body>
+	</html>
