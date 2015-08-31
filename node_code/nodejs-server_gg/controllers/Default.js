89494aac87707e926ddeb233b14035d5a9d68a4f
guidedgateway <rsaha@xmapledatalab.com>
2015-08-30 23:02:55 -0700

    API impl

--- node_code/nodejs-server_gg/controllers/Default.js
+++ node_code/nodejs-server_gg/controllers/Default.js
@@ -2,17 +2,22 @@
 
 var url = require('url');
 
-var Default = require('./DBService');
+
+var Default = require('./DefaultService');
 
 
 module.exports.guideGet = function guideGet (req, res, next) {
   var id = req.swagger.params['id'].value;
+  
 
-  res.setHeader('Content-Type', 'application/json');
-  Default.guideGet(id , function(result){
-    console.log(result);
+  var result = Default.guideGet(id);
+
+  if(typeof result !== 'undefined') {
+    res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify(result || {}, null, 2));
-  });
+  }
+  else
+    res.end();
 };
 
 module.exports.guidesGet = function guidesGet (req, res, next) {
@@ -21,33 +26,14 @@ module.exports.guidesGet = function guidesGet (req, res, next) {
   var theme = req.swagger.params['theme'].value;
   
 
-  res.setHeader('Content-Type', 'application/json');
-  Default.guidesGet(size, city, theme , function(result){
-    console.log(result);
-    res.end(JSON.stringify(result || {}, null, 2));
-  });
-};
+  var result = Default.guidesGet(size, city, theme);
 
-module.exports.tourGet = function tourGet (req, res, next) {
-  var tourid = req.swagger.params['tourid'].value;
-  
-  res.setHeader('Content-Type', 'application/json');
-  var tourid = req.swagger.params['tourid'].value;
-  Default.tourGet(tourid, function(result){
-    console.log(result);
+  if(typeof result !== 'undefined') {
+    res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify(result || {}, null, 2));
-  });
-};
-module.exports.toursGet = function toursGet (req, res, next) {
-  var size = req.swagger.params['size'].value;
-  var city = req.swagger.params['city'].value;
-  var theme = req.swagger.params['theme'].value;
-  
-  res.setHeader('Content-Type', 'application/json');
-  Default.toursGet(size, city , theme , function(result){
-    console.log(result);
-    res.end(JSON.stringify(result || {}, null, 2));
-  });
+  }
+  else
+    res.end();
 };
 
 module.exports.placeGet = function placeGet (req, res, next) {
@@ -86,6 +72,36 @@ module.exports.searchGet = function searchGet (req, res, next) {
   
 
   var result = Default.searchGet(size, keyword, criteria);
+
+  if(typeof result !== 'undefined') {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(result || {}, null, 2));
+  }
+  else
+    res.end();
+};
+
+module.exports.tourGet = function tourGet (req, res, next) {
+  var tourid = req.swagger.params['tourid'].value;
+  
+
+  var result = Default.tourGet(tourid);
+
+  if(typeof result !== 'undefined') {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(result || {}, null, 2));
+  }
+  else
+    res.end();
+};
+
+module.exports.toursGet = function toursGet (req, res, next) {
+  var size = req.swagger.params['size'].value;
+  var city = req.swagger.params['city'].value;
+  var theme = req.swagger.params['theme'].value;
+  
+
+  var result = Default.toursGet(size, city, theme);
 
   if(typeof result !== 'undefined') {
     res.setHeader('Content-Type', 'application/json');
