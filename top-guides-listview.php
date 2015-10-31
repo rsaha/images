ef4af7a98ebbbaf5dfc98d04b42f16825cd9bb24
Rakesh Saha <rakesh.x.saha@gmail.com>
2015-10-31 13:54:04 -0700

    changes

--- top-guides-listview.php
+++ top-guides-listview.php
@@ -179,13 +179,7 @@ ul.rating {
 										<ul class="list-unstyled">
 											<li ng-repeat="x in alltours" ng-show="$index<4">
 												<span class="rc-post-image">
-<<<<<<< HEAD
-                                                <a href="tour_detail_sidebar.php?id={{x.tour_id}}">
-													<img class="img-responsive" src="{{'x.photo' ==''||'x.photo' ? 'img/SAMPLE_TOUR.jpg' : x.photo}}" alt="Tour 1" />
-                                                    </a>
-=======
                                                     <a href="tour_detail_sidebar.php?id={{x.tour_id}}"><img class="img-responsive" src="{{'x.photo' ==''||'x.photo' ? 'img/SAMPLE_TOUR.jpg' : x.photo}}" alt="Tour 1" /></a>
->>>>>>> 81405ae27d8d970a4eb427a5e2b0ad4ac760d49a
 												</span>
 												<h5>{{x.tour_title}}</h5>
 												<span class="rc-post-date small">Starting Price&nbsp;{{x.tour_price}}</span><br/>
