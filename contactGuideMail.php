fec8d7ae283ade46e2bf8866010acbacfd758c15
Rakesh Saha <rakesh.x.saha@gmail.com>
2015-11-16 11:54:56 -0800

    few changes

--- contactGuideMail.php
+++ contactGuideMail.php
@@ -10,14 +10,14 @@
 		include_once('sendEmail.php');
 		$HostEmail = parse_ini_file('config.ini',true)['email'];
 		$subject = "Interested in custom tour";
-		$message    = "Tourist is looking for a tour and interested to contact through mail:<b><br />" . $Email . "</b><br /><br /> -----------------------------<br />";
+		$message    = "Tourist is looking for a custom tour with guide <b><br />" . $Email . "</b><br /><br /> -----------------------------<br />";
 		
 		//function SendMail(fromAddress, fromName, toAddress, toName, subject, message)
 		if(SendMail($HostEmail, 'GuidedGateway', 'support@guidedgateway.com', 'Guided Gateway Support', $subject, $message))
 		{
 			$errormsg="Acknowledgement email sent.";
 			error_log($errormsg,0);
-				if(SendMail($HostEmail, 'GuidedGateway', $Email, 'Tourist','thanx for contact', 'Thanx to contact with guided gateway team we will contact you soon'))
+				if(SendMail($HostEmail, 'GuidedGateway', $Email, 'Tourist','thanx for contact', 'Thanks for contacting us for your personalized custom tour. Our customer service team will reach out to you.'))
 				{
 					$errormsg="Acknowledgement email sent.";
 					error_log($errormsg,0);
