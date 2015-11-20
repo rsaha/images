0372f05bf100f910a2ba50d246ea0cc36416be67
Rakesh Saha <rakesh.x.saha@gmail.com>
2015-11-20 09:24:09 -0800

    merge

--- destination-detail-sidebar.php
+++ destination-detail-sidebar.php
@@ -41,7 +41,7 @@
 		<link rel="stylesheet" type="text/css" href="css/colors/color1.css" title="color1" />
 		
 		 <script type="text/javascript"  src= "js/angular.min.js"></script>
-        <script type="text/javascript"  src="topPlaceDetail.js"></script>
+        <script type="text/javascript"  src="topPlacesDetail.js"></script>
         <style type="text/css">
 .rating {
     color: #a9a9a9;
